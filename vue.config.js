594cbefc8f0df78292adfaeda8bf296fa99e09d6
bbaz <bbaz@technodom.kz>
2019-05-15 17:47:13 +0600

    multiplechoise added

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
     devServer: {
         compress: true,
         disableHostCheck: true,
+        port: 8000
     },
     publicPath: process.env.NODE_ENV === 'production'
         ? '/survey-td/'
