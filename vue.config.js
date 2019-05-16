c45a6ac56b32fb6f3ddfb44659f510900a7807eb
bbaz <bbaz@technodom.kz>
2019-05-16 11:30:28 +0600

    white page fixing

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,9 @@ module.exports = {
         disableHostCheck: true,
         port: 8000
     },
+    pwa: {
+        appleMobileWebAppCapable: 'yes'
+    },
     publicPath: process.env.NODE_ENV === 'production'
         ? '/survey-td/'
         : '/'
